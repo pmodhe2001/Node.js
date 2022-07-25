65ded78353a5bdcf90def244f5edb5696d888d5a
Prathmesh Modhe <pmodhe19@gmail.com>
2022-07-25 19:41:08 +0530

    added comments

--- 3.basichttpserver/index.js
+++ 3.basichttpserver/index.js
@@ -1,8 +1,18 @@
 // npm init on terminal after navigating through folder
-const http=require('http');
-const port=8000;//Whenever your system sends a request to a port (80)
+// Node js has different modules for different functionalities
+// first we 'll use http module
+
+const http=require('http'); // to call functionalities of http module  
+
+// 127.0.0/ is equals to local host
+const port=8000;//Whenever your system sends a request to a port (80 is default)
+// address of our server is localhost:800 or 127.0.0/8000
+
+// fs module is used to read/write in files
 const fs=require('fs');// load fs module
 
+
+//Request handler function by default takes 2 arguments request and response
 function requestHandler(req,res){
     console.log(req.url);
     res.writeHead(200,{'content-type':'text/html'});
@@ -16,9 +26,11 @@ function requestHandler(req,res){
         
     });
 }
-const server=http.createServer(requestHandler);
 
-server.listen(port,function(error){
+//request handler is called here
+const server=http.createServer(requestHandler);
+              
+server.listen(port,function(error){ // first argument corresponds to error
     if(error){
         console.log(error);
         return;
